286d856e8e7c7e794cfd0080fc76fa1e6ba2ad02
wlee <tree03@paran.com>
2012-06-11 13:47:01 -0500

    added unit tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -20,7 +20,7 @@ To test the program:
 import StringIO
 import unittest
 
-from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
+from Collatz import collatz_read, collatz_eval, collatz_getCycLen, collatz_print, collatz_solve
 
 # -----------
 # TestCollatz
@@ -63,7 +63,33 @@ class TestCollatz (unittest.TestCase) :
     # my eval
     # -------
     
+    def test_my_eval_1 (self) :
+        v = collatz_eval(1, 666)
+        self.assert_(v == 145)
+
+    def test_my_eval_2 (self) :
+        v = collatz_eval(1, 999)
+        self.assert_(v == 179)
+
+    def test_my_eval_3 (self) :
+        v = collatz_eval(128, 256)
+        self.assert_(v == 128)
+
+    # ---------
+    # getCycLen
+    # ---------
     
+    def test_getCycLen_1 (self) :
+        v = collatz_getCycLen(1)
+        self.assert_(v == 1)
+
+    def test_getCycLen_2 (self) :
+        v = collatz_getCycLen(5)
+        self.assert_(v == 6)
+
+    def test_getCycLen_3 (self) :
+        v = collatz_getCycLen(10)
+        self.assert_(v == 7)
 
     # -----
     # print
